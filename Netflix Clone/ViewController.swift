c32d544f9d5d24ca558c34e87f682679992523f7
samet.egerci <161208667+sametegerci@users.noreply.github.com>
2024-07-29 18:55:07 +0300

    Initial Commit

---
+++ Netflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by  Samet Eğerci on 29.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
